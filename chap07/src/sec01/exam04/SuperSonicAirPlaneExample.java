ea63b50b9af3cb92b3cbcc4ae3a4ae8e2944128e
SeoYeongun <voe697754@gmail.com>
2024-01-16 15:28:57 +0900

    chap07.sec01.exam07 까지

---
+++ chap07/src/sec01/exam04/SuperSonicAirPlaneExample.java
@@ -0,0 +1,16 @@
+package sec01.exam04;
+
+public class SuperSonicAirPlaneExample {
+
+	public static void main(String[] args) {
+		SupersonicAirPlane sa = new SupersonicAirPlane();
+		sa.takeOff();
+		sa.fly();
+		sa.flyMode = SupersonicAirPlane.SUPERSONIC;
+		sa.fly();
+		sa.flyMode = SupersonicAirPlane.NORMAL;
+		sa.fly();
+		sa.land();
+	}
+
+}
---
+++ chap07/src/sec01/exam06/SportsCar.java
@@ -0,0 +1,10 @@
+package sec01.exam06;
+
+public class SportsCar extends Car{
+
+	@Override
+	public void speedUp() {
+		speed += 10;
+	}
+	
+}
